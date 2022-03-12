829414dd85f464b09886d0538f6d5100b1772cf8
Nick Mazey <nicholasmazey@gmail.com>
2022-03-13 12:12:58 +1300

    fixed checkerframework script, it now fails successfully

--- checkerframeworkscript.bat
+++ checkerframeworkscript.bat
@@ -1,2 +1,2 @@
 echo "" > log/checkerframework.log
-mvn clean package --log-file log/checkerframework.log -P checkerframework-jdk8
+mvn clean package --log-file log/checkerframework.log -P java8compile,checkerframework
