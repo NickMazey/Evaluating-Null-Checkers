9831db6aaa215b7a70bbedc43b2027a06fce125b
NickMazey <nicholasmazey@gmail.com>
2022-04-21 18:13:49 +1200

    added automatic detection of infer location

--- inferscript.bat
+++ inferscript.bat
@@ -1,5 +1,8 @@
 bench_file=$(basename $1)
 bench_name="${bench_file%.*}"
 echo "" > log/infer/$bench_name.log
-infer run --quiet --debug --eradicate --pulse -- mvn clean compile -Dbenchmark=$1 -Djavaver=11 -Dcheckerframework=false --log-file log/infer/$bench_name.log
-cp -v infer-out/report.txt log/infer/"$bench_name"_report.txt
+#Finding infer location for use in maven
+infer_location=`which infer`
+infer run --quiet --debug --eradicate --pulse  -- mvn clean compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file log/infer/$bench_name.log
+#Suppresses output
+sent=`cp -v infer-out/report.txt log/infer/"$bench_name"_report.txt`
