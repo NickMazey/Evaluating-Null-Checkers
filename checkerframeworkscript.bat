de4203af443d3614aa5dad27f6890e81b5c78e24
NickMazey <nicholasmazey@gmail.com>
2022-04-28 17:21:21 +1200

    updated scripts to use Java 11

--- checkerframeworkscript.bat
+++ checkerframeworkscript.bat
@@ -1,4 +1,4 @@
 bench_file=$(basename $1)
 bench_name="${bench_file%.*}"
 echo "" > log/checkerframework/$bench_name.log
-mvn clean compile -Dbenchmark=$1 -Djavaver=11 -Dcheckerframework=true --log-file log/checkerframework/$bench_name.log -P java8compile,checkerframework
+mvn clean compile -Dbenchmark=$1 -Djavaver=11 -Dcheckerframework=true --log-file log/checkerframework/$bench_name.log -P java11compile,checkerframework
