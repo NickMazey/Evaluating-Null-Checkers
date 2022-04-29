5ac8dd2ac1489640441e1b4e8280251751957b26
NickMazey <nicholasmazey@gmail.com>
2022-04-29 13:21:41 +1200

    Moved mvn clean command into run sh for testing speed improvements

--- inferscript.sh
+++ inferscript.sh
@@ -5,6 +5,6 @@ echo "" > log/infer/$bench_name.log
 rm -r infer-out
 #Finding infer location for use in maven
 infer_location=`which infer`
-infer run --quiet --debug --eradicate --pulse  -- mvn clean compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file log/infer/$bench_name.log
+infer run --quiet --debug --eradicate --pulse  -- mvn compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file log/infer/$bench_name.log
 #Suppresses output
 sent=`cp -v infer-out/report.txt log/infer/"$bench_name"_report.txt`
