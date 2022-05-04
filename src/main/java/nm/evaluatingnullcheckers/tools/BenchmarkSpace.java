a173bf1c7871991a81fea8b3f057d907f542ab9d
NickMazey <nicholasmazey@gmail.com>
2022-05-04 16:39:12 +1200

    changed output stream and added CSV output for missing benchmarks

--- src/main/java/nm/evaluatingnullcheckers/tools/BenchmarkSpace.java
+++ src/main/java/nm/evaluatingnullcheckers/tools/BenchmarkSpace.java
@@ -1,14 +1,17 @@
 package nm.evaluatingnullcheckers.tools;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 
 import nm.evaluatingnullcheckers.benchmarks.*;
 
@@ -19,15 +22,28 @@ public class BenchmarkSpace {
 	private ArrayList<Class<?>> annotationTypes;
 	private HashMap<Class<?>,ArrayList<Class<?>>> annotationSubTypes;
 	
+	private PrintStream outputStream;
+	
 	public BenchmarkSpace() {
+		this(System.out);
+	}
+	
+	public BenchmarkSpace(PrintStream outStream) {
+		loadAnnotationTypes();
+		this.outputStream = outStream;
+	}
+	
+	private void loadAnnotationTypes() {
 		annotationTypes = new ArrayList<Class<?>>();
 		annotationTypes.add(BenchmarkAnnotations.Annotation.class);
 		annotationTypes.add(BenchmarkAnnotations.AnalysisScope.class);
 		annotationTypes.add(BenchmarkAnnotations.VariableScope.class);
 		annotationTypes.add(BenchmarkAnnotations.VariableType.class);
 		annotationTypes.add(BenchmarkAnnotations.ExpectedNPE.class);
-
-		
+		loadAnnotationSubTypes();
+	}
+	
+	private void loadAnnotationSubTypes() {
 		annotationSubTypes = new HashMap<Class<?>,ArrayList<Class<?>>>();
 		for (Class<?> clazz : annotationTypes) {
 			annotationSubTypes.put(clazz, new ArrayList<Class<?>>());
@@ -44,39 +60,91 @@ public class BenchmarkSpace {
 				}
 			}
 		}
-		
+	}
+	
+	public void outputMissingBenchmarksToCSV() throws Exception{
+		outputMissingBenchmarksToCSV("missing.csv");
+	}
+	
+	public void outputMissingBenchmarksToCSV(String fileName) throws Exception{
+		outputMissingBenchmarksToCSV(new File(fileName));
+	}
+	
+	public void outputMissingBenchmarksToCSV(File file) throws Exception {
+		HashSet<Class<?>> benchmarks = getAllBenchmarkClasses();
+        HashSet<String> annotationCombinations = getPresentAnnotationCombinations(benchmarks);
+        HashSet<String> missingCombinations = this.findMissingCombinations(annotationCombinations);
+        String toWrite = this.combinationSetToCSV(missingCombinations);
+		FileWriter writer = new FileWriter(file);
+		writer.write(toWrite);
+		writer.flush();
+		writer.close();
+		outputStream.println("missing combinations written to " + file.getAbsolutePath());
 		
 	}
 	
-	public void visualiseBenchmarks() {
+	public void printBenchmarkInfo() {
 		//Count how many combinations there are
 		int combinations = 1;
 		for(Class<?> clazz : annotationTypes) {
 			int number = annotationSubTypes.get(clazz).size();
 			combinations *= number;
-			System.out.println("For annotation type " + clazz.getSimpleName() + " there are " + number + " subtypes" );
+			outputStream.println("For annotation type " + clazz.getSimpleName() + " there are " + number + " subtypes" );
 		}
-		System.out.println("There are a total of " + combinations + " possible combinations");
-		
 		HashSet<Class<?>> benchmarks = getAllBenchmarkClasses();
-
-        
         HashSet<String> annotationCombinations = getPresentAnnotationCombinations(benchmarks);
-        
-        
-        System.out.println("Of " + combinations + " possible annotation combinations, " + annotationCombinations.size() + " could be found");
-        
-        
-        HashSet<String> missingCombinations = getAllPossibleAnnotationCombinations();
-        for(String combination : annotationCombinations) {
-        	missingCombinations.remove(combination);
+        outputStream.println("Of " + combinations + " possible unique annotation combinations, " + annotationCombinations.size() + " could be found");            
+	}
+	
+	private HashSet<String> findMissingCombinations(HashSet<String> foundCombinations){
+		HashSet<String> allCombinations = getAllPossibleAnnotationCombinations();
+		for (String combination : foundCombinations) {
+			allCombinations.remove(combination);
+		}
+		return allCombinations;
+	}
+	
+	private String combinationSetToPrettyString(HashSet<String> combinations) {
+		StringBuilder prettyCombinationSet = new StringBuilder();
+		for(String combination : combinations) {
+			prettyCombinationSet.append(combinationToPrettyString(combination));
+			prettyCombinationSet.append("\n");
+		}
+		return prettyCombinationSet.toString();
+	}
+	
+	private String combinationSetToCSV(HashSet<String> combinations) {
+		StringBuilder combinationCSV = new StringBuilder();
+        // Column titles
+        for(int i = 0; i < annotationTypes.size(); i++) {
+        	if(i != 0) {
+        		combinationCSV.append(",");
+        	}
+        	combinationCSV.append(annotationTypes.get(i).getSimpleName());
         }
+        combinationCSV.append("\n");
         
-        for(String str : missingCombinations) {
-        	
+        //Content
+        for(String combination : combinations) {
+        	combinationCSV.append(combinationToCSVLine(combination));
+        	combinationCSV.append("\n");
         }
-        
-        
+		return combinationCSV.toString();
+	}
+	
+	private String combinationToCSVLine(String combination) {
+		return combinationToCSVLine(combinationToArray(combination));
+	}
+	
+	private String combinationToCSVLine(ArrayList<String> combination) {
+		StringBuilder CSVLine = new StringBuilder();
+		for(int i = 0; i < combination.size(); i++) {
+			if(i != 0) {
+				CSVLine.append(",");
+			}
+			CSVLine.append(combination.get(i));
+		}
+		return CSVLine.toString();
 	}
 	
 	private String combinationToPrettyString(ArrayList<String> combination) {
@@ -113,7 +181,7 @@ public class BenchmarkSpace {
 		return new ArrayList<String>(Arrays.asList(annotations));
 	}
 	
-	private HashSet<Class<?>> getAllBenchmarkClasses(){
+	public static HashSet<Class<?>> getAllBenchmarkClasses(){
 		HashSet<Class<?>> benchmarks = new HashSet<Class<?>>();
 		String packageName = "nm.evaluatingnullcheckers.benchmarks";
         InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(packageName.replaceAll("[.]", "/"));
@@ -175,8 +243,6 @@ public class BenchmarkSpace {
 			currentPerms = newPerms;
 		}
 		return currentPerms;
-		
-		
 	}
 		
 	private HashSet<String> getAllValuesForIndex(String base, int index, int numValues){
@@ -190,9 +256,15 @@ public class BenchmarkSpace {
 		return combinations;
 	}
 	
+	
 	public static void main(String[] args) {
 		BenchmarkSpace bench = new BenchmarkSpace();
-		bench.visualiseBenchmarks();
+		bench.printBenchmarkInfo();
+		try{
+			bench.outputMissingBenchmarksToCSV();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 	}
 
 }
