db49c4ee6a539849705026cbb670a528159b7c46
NickMazey <nicholasmazey@gmail.com>
2022-06-10 12:50:09 +1200

    made run.sh autorun both checkers and output parser

--- inferscript.sh
+++ inferscript.sh
@@ -5,6 +5,7 @@ log_location=$2
 rm -r infer-out
 #Finding infer location for use in maven
 infer_location=`which infer`
-infer run --quiet --debug --eradicate --pulse  -- mvn compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file $log_location/infer/$bench_name.log
+#To fix a weird bug where it says "java_class_info should be present for Java classes" while still working fine
+silence_infer=$(infer run --quiet --debug --eradicate --pulse  -- mvn compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file $log_location/infer/$bench_name.log)
 #Suppresses output
 sent=`cp -v infer-out/report.txt $log_location/infer/"$bench_name".inferreport`
--- runall.sh
+++ runall.sh
@@ -1,1 +1,10 @@
-./run.sh -c allcheckers.txt -b allbenchmarks.txt -l log/log"$(date +%Y%m%dT%H%M%S)"
+timestamp="$(date +%Y%m%dT%H%M%S)"
+./run.sh -c allcheckers.txt -b allbenchmarks.txt -l log/log$timestamp
+#This isn't proper but it works
+silent="$(mvn dependency:build-classpath -Dmdep.outputFile=classpath.txt)"
+classpath="$(cat classpath.txt)"
+loglocation="$(realpath log)"
+silent="$(mvn clean compile -Dcompiletools=true)"
+java -cp target/classes:$classpath nm.evaluatingnullcheckers.tools.CheckerOutputParser $loglocation/log$timestamp $loglocation/results$timestamp.json
+rm classpath.txt
+echo "output available at $loglocation/results$timestamp.json"
