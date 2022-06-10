cc1be9ed206277b91aefec61f9b7fc97e0bb4a5a
NickMazey <nicholasmazey@gmail.com>
2022-06-10 12:55:20 +1200

    improved inferscript to remove weird bug

--- inferscript.sh
+++ inferscript.sh
@@ -6,6 +6,6 @@ rm -r infer-out
 #Finding infer location for use in maven
 infer_location=`which infer`
 #To fix a weird bug where it says "java_class_info should be present for Java classes" while still working fine
-infer run --quiet --debug --eradicate --pulse  -- mvn compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file $log_location/infer/$bench_name.log
+infer run --quiet --debug --no-default-checkers --eradicate --pulse  -- mvn compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file $log_location/infer/$bench_name.log
 #Suppresses output
 sent=`cp -v infer-out/report.txt $log_location/infer/"$bench_name".inferreport`
