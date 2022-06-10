03e96ff1a7cb18d5b63e51ec53b9912f9b13a113
NickMazey <nicholasmazey@gmail.com>
2022-06-10 15:34:33 +1200

    fixed issue that caused jvm core dump

--- checkerframeworkscript.sh
+++ checkerframeworkscript.sh
@@ -1,5 +1,5 @@
 bench_file=$(basename $1)
 bench_name="${bench_file%.*}"
 log_location=$2
-echo "" > "$log_location/checkerframework/$bench_name.log"
-mvn compile -Dbenchmark="$1" -Djavaver=11 -Dcheckerframework=true --log-file "$log_location/checkerframework/$bench_name.log" -P java11compile,checkerframework
+echo "" > $log_location/checkerframework/$bench_name.log
+mvn compile -Dbenchmark=$1 -Djavaver=11 -Dcheckerframework=true --log-file $log_location/checkerframework/$bench_name.log -P java11compile,checkerframework
--- inferscript.sh
+++ inferscript.sh
@@ -1,11 +1,11 @@
 bench_file=$(basename $1)
 bench_name="${bench_file%.*}"
 log_location=$2
-echo "" > "$log_location/infer/$bench_name.log"
+echo "" > $log_location/infer/$bench_name.log
 rm -r infer-out
 #Finding infer location for use in maven
 infer_location=`which infer`
 #To fix a weird bug where it says "java_class_info should be present for Java classes" while still working fine
-infer run --quiet --debug --no-default-checkers --eradicate --pulse  -- mvn compile -Dbenchmark="$1" -Djavaver=11 -Dinferlocation="$infer_location" -Dcheckerframework=false --log-file "$log_location/infer/$bench_name.log"
+infer run --quiet --debug --no-default-checkers --eradicate --pulse  -- mvn compile -Dbenchmark=$1 -Djavaver=11 -Dinferlocation=$infer_location -Dcheckerframework=false --log-file $log_location/infer/$bench_name.log
 #Suppresses output
-sent=`cp -v infer-out/report.txt "$log_location/infer/"$bench_name".inferreport"`
+sent=`cp -v infer-out/report.txt $log_location/infer/"$bench_name".inferreport`
