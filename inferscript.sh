25403c54965b04dcd7e65216ea50e309d780b193
NickMazey <nicholasmazey@gmail.com>
2022-06-17 13:59:28 +1200

    added -f flag to force rm

--- inferscript.sh
+++ inferscript.sh
@@ -2,7 +2,7 @@ bench_file=$(basename $1)
 bench_name="${bench_file%.*}"
 log_location=$2
 echo "" > $log_location/infer/$bench_name.log
-rm -r infer-out
+rm -r -f infer-out
 #Finding infer location for use in maven
 infer_location=`which infer`
 #To fix a weird bug where it says "java_class_info should be present for Java classes" while still working fine
